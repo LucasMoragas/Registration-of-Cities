69aaa88c332dfa8c2dda402c3493599961b9cfb0
LucasMoragas <moragaslucas08@gmail.com>
2022-11-19 21:20:32 -0300

    Creation of the link class

---
+++ RegistrationOfCities/src/link/Link.java
@@ -0,0 +1,24 @@
+package link;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lucas Moragas Melo Oliveira
+ */
+public class Link {
+    //The function will establish a connection to the database
+    public static Connection getConnection(){
+        Connection connection = null;
+        
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cities","root","oeste0808");//jbc (java database connector)
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "CONNECTION ERROR");
+        }
+        
+        return connection;
+    }
+}
